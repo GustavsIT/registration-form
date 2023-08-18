f8a77473276c5da516b196d809bc0f3fe1f2faea
GUSIX <gustavs.petris98@gmail.com>
2023-08-18 12:58:25 +0300

    implemented form data in db.json and data visible in the form

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,56 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios from "axios";
 
-console.log('Ready for coding');
+const form = document.querySelector<HTMLFormElement>('.js-form')
+const posts = document.querySelector('.js-posts')
+const submitButton = document.querySelector<HTMLButtonElement>('.button-container')
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+type Post = {
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    gender: string;
+    selectcountry: string;
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitButton.disabled=true;
+
+    const formData = new FormData(form);
+
+    const finalData : {[key :string ]:unknown}={};
+
+    for(var pair of formData.entries()){
+        finalData[pair[0]]=pair[1];
+    }
+    console.log(finalData)
+
+    axios.post('http://localhost:3004/posts',finalData).then((res) => {
+
+       const post :Post = res.data;
+            
+        const postHTML = `<div>
+        <h1>${post.firstname}</h1>
+        <p>${post.lastname}</p>
+        <p>${post.email}</p>
+        <p>${post.password}</p>
+        <p>${post.gender}</p>
+        <p>${post.selectcountry}</p>
+        </div><br><br>`;
+
+        posts.innerHTML += postHTML;
+        form.reset;
+        submitButton.disabled=false;
+    });
+    
+
+
+});
+
+
+
+
+
+
+
